913d8865366b029b20e5758280f231438fface20
Andrew Freedman <ajfreedm@gmail.com>
2022-01-08 13:04:26 -0500

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Account.destroy_all
+User.destroy_all
+
+@admin = User.create!(username: "admin", email: 'ajfreedm@gmail.com', password: '123456')
+
+puts "#{User.count} users created"
+
+Account.create!(category: "checking", balance: "1000", user: @admin)
+Account.create!(category: "savings", balance: "5000", user: @admin)
+
+puts "#{Account.count} accounts created"
