1b86dad04326c2e366c16c3080416b2106783fd8
Andrew Freedman <ajfreedm@gmail.com>
2022-04-21 17:10:00 -0400

    Logout api call

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,11 +1,20 @@
 import { Link }from 'react-router-dom'
 
 export default function Layout(props) {
+    const {currentUser, handleLogout} = props;
+
     return (
         <div>
             <header>
         <h1>Freedman-Banking</h1>
-        <Link to='/login'>Login/Register</Link>
+        {currentUser ? (
+            <div
+            ><p>{currentUser.username}</p>
+            <button onClick={handleLogout}>Logout</button>
+            </div>
+        ) : (
+            <Link to='/login'>Login/Register</Link>
+        )}
             </header>
             {props.children}    
         </div>
